488eca4b9f2def63a181de7d80a1c2138b490bc2
ambikan9611 <113337175+ambikan9611@users.noreply.github.com>
2022-09-20 07:56:56 +0530

    part b prg

---
+++ prga2/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=2;
+    float b=2;
+    char c=3;
+    long int d=4;
+    unsigned long int e=7;
+    printf("intger%d\n",a);
+    printf("float%f\n",b);
+    printf("character%c\n",c);
+    printf("long int%ld\n",d);
+    printf("unsigned long int%u",e);
+
+    return 0;
+}
+/*/
+intger2
+float2.000000
+character♥
+long int4
+unsigned long int7
+Process returned 0 (0x0)   execution time : 0.047 s
+Press any key to continue.
+/*/
+
