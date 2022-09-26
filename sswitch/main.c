76d28923011bb594ace40aeceac503d3b9a1e5e3
ambikan9611 <113337175+ambikan9611@users.noreply.github.com>
2022-09-26 07:02:33 +0530

    switch

---
+++ sswitch/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+int a;
+int b;
+  void num_print()
+  {
+  printf("enter the value of a\n");
+  scanf("%d\n",&a);
+  b=a*a;
+  printf("%d",b);
+  }
+  int main()
+  {
+  num_print();
+  return 0;
+  }
