64232137e12d4a64fe24d99ed4cb6e6a3a663555
ambikan9611 <113337175+ambikan9611@users.noreply.github.com>
2022-09-26 08:26:11 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,43 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int product(int a,int b)
+int main()
 {
-  int product(int a,int b);
-  return product;
+    printf("ambika got new laptop hurry!\n");
+    return 0;
 }
-int add(int a,int b)
-{
-  int add(int a,int b);
-  return add;
-}
-int sub(int a,int b)
-{
-  int sub(int a,int b);
-  return sub;
-}
-int dive(int a,int b)
-{
-  int dive(int a,int b);
-  return dive;
-}
- int main(add1,sub,product,dive,a,b){
-   printf("enter the number of a b\n");
-   scanf("%d%d",&a,&b);
-   product=product(int a,int b);
-   add1=add(int a,int b);
-   sub=sub(int a,int b);
-   dive=dive(int a,int b);
-   printf("%d\n",product);
-   printf("%d\n",add);
-   printf("%d\n",sub);
-   printf("%d\n",dive);
-   return 0;
-}
-
-
-
-
-
-
-
---
+++ prgd13.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ int num1,num2;
+ printf("enter the num1");
+ scanf("%d",&num1);
+ printf("enter the num2");
+ scanf("%d",&num2);
+ ((num1==30)|(num2==30)|(num1+num2==30))?printf("%d",1):printf("%d",1);
+ return 0;
+}
