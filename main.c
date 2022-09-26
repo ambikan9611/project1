af6c64b372d8485b71dd33668300ccf42030109b
ambikan9611 <113337175+ambikan9611@users.noreply.github.com>
2022-09-26 07:05:44 +0530

    work4

--- main.c
+++ main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+
+int product(int a,int b)
 {
-    int a=2;
-    printf("the value is%d\n",a);
-    return 0;
+  int product(int a,int b);
+  return product;
 }
+int add(int a,int b)
+{
+  int add(int a,int b);
+  return add;
+}
+int sub(int a,int b)
+{
+  int sub(int a,int b);
+  return sub;
+}
+int dive(int a,int b)
+{
+  int dive(int a,int b);
+  return dive;
+}
+ int main(add1,sub,product,dive,a,b){
+   printf("enter the number of a b\n");
+   scanf("%d%d",&a,&b);
+   product=product(int a,int b);
+   add1=add(int a,int b);
+   sub=sub(int a,int b);
+   dive=dive(int a,int b);
+   printf("%d\n",product);
+   printf("%d\n",add);
+   printf("%d\n",sub);
+   printf("%d\n",dive);
+   return 0;
+}
+
+
+
+
+
+
+
